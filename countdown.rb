4f8c8a0f7fcccc0d605ca740cf994990cdf5f5fb
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-11 12:42:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
-
-def countdown
+def countdown(counter)
+   counter = 10 
+  while counter > 0 
+  puts "#{counter} SECONDS(S)"
+  counter -= 1 
+  puts "HAPPY NEW YEAR!"
 end
+countdown(counter)
+end 
