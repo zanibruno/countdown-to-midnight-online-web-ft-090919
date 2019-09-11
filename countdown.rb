ba5e9b0accec96c64f5729e5a209c90351eb1c18
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-11 12:51:25 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
-def countdown
+def countdown(counter)
+   counter = 10 
+  while counter > 0 
+  puts "#{counter} SECOND(S)!"
+  counter -= 1 
 end
+return "HAPPY NEW YEAR!"
+end 
+
+
+def countdown_with_sleep(counter)
+   counter = 10 
+  while counter > 0 
+  puts "#{counter} SECOND(S)!"
+  counter -= 1 
+  sleep(1)
+end
+return "HAPPY NEW YEAR!"
+end 
